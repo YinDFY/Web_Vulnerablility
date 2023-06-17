b324400701497f70a58d1fadc2df4c9a95df56a0
YinDFY <127073326+YinDFY@users.noreply.github.com>
2023-06-17 15:30:29 +0800

    Update 自动解析网页存在哪些表单并提取参数.py

--- 爬虫/自动解析网页存在哪些表单并提取参数.py
+++ 爬虫/自动解析网页存在哪些表单并提取参数.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-def getParatemater(url):
+def getParameter(url):
     response = requests.get(url)
     # 使用 BeautifulSoup 解析页面
     soup = BeautifulSoup(response.content, "html.parser")
@@ -13,16 +13,23 @@ def getParatemater(url):
         # 查找表单中的所有输入元素
         inputs = form.find_all("input")
 
-        # 打印表单参数
+        # 打印表单参数及其值
         for input in inputs:
             # 获取输入元素的 name 属性值
             param = input.get("name")
             if param:
+                # 获取输入元素的 value 属性值
+                value = input.get("value")
                 print("表单参数:", param)
+                print("参数值:", value)
+
 
 '''
 示例输出：
 表单参数: username
+参数值: None
 表单参数: password
+参数值: None
 表单参数: submit
+参数值: Login
 '''
