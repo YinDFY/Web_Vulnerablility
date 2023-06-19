bac74eed4aff646d80236bfa70476d940b3bf15c
YinDFY <127073326+YinDFY@users.noreply.github.com>
2023-06-19 11:57:42 +0800

    Update web_crawler.py

--- 爬虫/web_crawler.py
+++ 爬虫/web_crawler.py
@@ -4,6 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 from queue import Queue
+from urllib.parse import urljoin, urlparse
 
 def spider1_link(url):
     links = []
@@ -21,22 +22,31 @@ def spider1_content(url):
     html_content = response.text
     return html_content
 
+def is_same_origin(url1, url2):
+    parsed_url1 = urlparse(url1)
+    parsed_url2 = urlparse(url2)
+    return parsed_url1.netloc == parsed_url2.netloc
+
 def spider2_content(url):
+    base_url = url
     unvisited = Queue()
     links1 = spider1_link(url)
     for link in links1:
         unvisited.put(link)
     while not unvisited.empty():
         suburl = unvisited.get()
-        links2 = spider1_link(url+suburl)
-        for link in links2:
-            if link not in links1:
-                links1.append(link)
-                unvisited.put(link)
+        absolute_url = urljoin(base_url, suburl)  # 构建绝对URL
+        if is_same_origin(base_url, absolute_url):  # 判断是否同源
+            links2 = spider1_link(absolute_url)
+            for link in links2:
+                if link not in links1:
+                    links1.append(link)
+                    unvisited.put(link)
 
     unique_link = [x for i, x in enumerate(links1) if x not in links1[:i]]
     for link in unique_link:
-        print(link)
+        absolute_link = urljoin(base_url, link)  # 构建绝对URL
+        print(absolute_link)
 
 
 if __name__ == '__main__':
